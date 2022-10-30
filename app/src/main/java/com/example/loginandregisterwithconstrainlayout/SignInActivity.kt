156572cf2eb5c23db7b0dd7b737b31c51da6842f
Ifhrzy022 <mirgifhrzy@gmail>
2022-10-30 22:56:21 +0700

    Pertemuan ke5

---
+++ app/src/main/java/com/example/loginandregisterwithconstrainlayout/SignInActivity.kt
@@ -0,0 +1,22 @@
+package com.example.loginandregisterwithconstrainlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        supportActionBar?.hide()
+
+        toRegister()
+    }
+
+    private fun  toRegister() {
+        tv_register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
